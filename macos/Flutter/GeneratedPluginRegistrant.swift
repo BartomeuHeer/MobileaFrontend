23a494b3443ce54eefb86bcee95d4d0ccc0c436f
nigati <galicitiscenco41@gmail.com>
2023-01-28 11:04:24 +0100

    1

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import geolocator_apple
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
